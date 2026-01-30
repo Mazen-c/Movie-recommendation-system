07f526b0fb78d00ba60754f935464afb235157cd
Mazen-c <mazenschool1234@gmail.com>
2026-01-30 16:45:41 +0200

    Recommendation logic added

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-import pandas as pd 
-from src.recommender import build_similarity_matrix, recommend
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-movies = pd.read_csv(r"data\raw\ml-latest-small\movies.csv")
-ratings = pd.read_csv(r"data\raw\ml-latest-small\ratings.csv")
-similarity = build_similarity_matrix(movies)
+movies = pd.read_csv(r'C:\Projects\Movie recommendation system\data\raw\top10K-TMDB-movies.csv')
 
-print("\nGetting recommendations for 'Toy Story (1995)':")
-recommendations = recommend("Powder (1995)", movies, similarity)
-print(recommendations)
+print("=== DESCRIBE ===")
+print(movies.describe())
+
+print("\n=== INFO ===")
+print(movies.info())
+
+print("\n=== NULLS ===")
+print(movies.isnull().sum())
+
+movies = movies[['id', 'title', 'genre', 'overview']]
+
+print("\n=== SELECTED FEATURES ===")
+print(movies.head())
+
+
+movies['tags'] = movies['genre']+movies['overview']
+print(movies.head())
+
+new_data = movies.drop(columns=['genre' , 'overview'])
+print("\n=== NEW DATA ===")
+print(new_data.head())
+
+cv = CountVectorizer(max_features=10000, stop_words='english')
+vector = cv.fit_transform(new_data['tags'].values.astype('U')).toarray()
+print(vector.shape)
+similairty = cosine_similarity(vector)
+
+def recommend(title):
+    index =new_data[new_data['title'] == title].index[0]
+    distance= sorted(list(enumerate(similairty[index])) , reverse=True, key=lambda vector: vector[1])
+    for i in distance[0:5]:
+        print(new_data.iloc[i[0]].title)
